c4ecc3f1546621fc8aad434bbd47d23c8812d11e
Даниил Щукарев <dashch31@gmail.com>
2026-06-18 12:14:55 +0400

    scripts

---
+++ scripts/src/main/java/my/scripts/ScriptsApplication.java
@@ -0,0 +1,13 @@
+package my.scripts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScriptsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScriptsApplication.class, args);
+    }
+
+}
