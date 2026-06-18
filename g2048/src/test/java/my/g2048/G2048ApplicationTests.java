732d033675137fb8cfbf5bda176d36a427061259
Даниил Щукарев <dashch31@gmail.com>
2026-06-18 12:13:02 +0400

    G2048

---
+++ g2048/src/test/java/my/g2048/G2048ApplicationTests.java
@@ -0,0 +1,13 @@
+package my.g2048;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class G2048ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
